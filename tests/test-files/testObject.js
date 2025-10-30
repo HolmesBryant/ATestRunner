6dcaf5b5198a937b36052d7dce6434ca4b2ee9cb
Holmes Bryant <webbmaastaa@gmail.com>
2025-10-30 13:41:03 -0500

    updated documentation

--- tests/test-files/testObject.js
+++ tests/test-files/testObject.js
@@ -4,10 +4,26 @@ export default {
 	baz: undefined,
 	arr: [1,2,3],
 	pojo: {a:1, b:2, c:3},
+	getArr() { return this.arr },
+	getPojo() { return this.pojo },
 	async asyncFunc(arg) {
 		return new Promise((resolve, reject) => {
   		setTimeout(() => { resolve(arg ?? 'foo') }, 300);
 		});
 	},
-	getArr() { return this.arr }
+	insertElem(tag) {
+		const elem = document.createElement(tag);
+		document.body.append(elem);
+	},
+	get code() {
+  	const tempObj = this;
+  	delete tempObj.code;
+	  const replacer = (key, value) => {
+	    if (typeof value === 'function') {
+	      return value.toString();
+	    }
+	    return value;
+	  };
+	  return JSON.stringify(tempObj, replacer, 2);
+	}
 };
---
+++ tests/testobject.tests.js
@@ -0,0 +1,90 @@
+import obj from './test-files/testObject.js';
+import ATestRunner from '../src/ATestrunner.js';
+const runner = new ATestRunner(import.meta.url)
+const {equal, info, test, wait, when} = runner;
+
+function insertElem(tag) {
+	const elem = document.createElement(tag);
+	document.body.append(elem);
+}
+
+info("Testing testObject")
+
+info("--- Basic Properties ---")
+
+test(
+	"foo should be 'foo'",
+	obj.foo,
+	'foo'
+);
+
+test(
+	'bar should be null',
+	obj.bar,
+	null
+);
+
+test(
+	"baz should be undefined",
+	obj.baz,
+	undefined
+);
+
+test(
+	"arr should be an Array",
+	Array.isArray(obj.arr),
+	true
+);
+
+test(
+	"pojo.a should be 1",
+	obj.pojo.a,
+	1
+);
+
+info("--- methods ---");
+
+test(
+	"getArr() should return [1,2,3]",
+	equal(obj.getArr(), [1,2,3]),
+	true
+);
+
+test(
+	"getPojo should return {a:1, b:2, c:3}",
+	equal(obj.getPojo(), {a:1, b:2, c:3}),
+	true
+);
+
+test(
+	"asyncFunc() should return 'foo'",
+	async () => await obj.asyncFunc(),
+	'foo'
+);
+
+test(
+	"testing insertElem('pre') with when()",
+	async () => {
+		insertElem('pre');
+		await when(document.body.querySelector('pre'));
+		const el = document.body.querySelector('pre');
+		el.remove();
+		return el instanceof HTMLPreElement;
+	},
+	true
+)
+
+test(
+	"testing insertElem('pre') with wait()",
+	async () => {
+		insertElem('pre');
+		wait(10);
+		const el = document.body.querySelector('pre');
+		el.remove();
+		return el instanceof HTMLPreElement;
+	},
+	true
+)
+
+runner.run();
+
